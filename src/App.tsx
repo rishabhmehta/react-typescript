e4d52ddfd1a9da8d9fe5bf311798ceb524aab6b6
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-21 13:07:39 +0530

    polymorphic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css"
-import { CustomButton } from "./components/html/Button"
+import { Text } from "./components/polymorphic/Text"
 
 function App() {
   return (
     <div className="App">
-      <CustomButton variant="primary" onClick={() => console.log("Clicked")}>
-        Primary button
-      </CustomButton>
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="h2" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   )
 }
